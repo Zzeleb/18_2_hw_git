ef7a3285f753d73f02786d9f3f19e639c5a98a54
Zzeleb <113052661+Zzeleb@users.noreply.github.com>
2023-02-22 21:18:01 +0300

    Update Main.java

--- src/main/java/ru/zzeleb/Main.java
+++ src/main/java/ru/zzeleb/Main.java
@@ -2,6 +2,6 @@ package ru.zzeleb;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello everybody!");
     }
 }
